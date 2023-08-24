04a680803e4abec1ee073165b39259493790769b
Ram Goel <ramgoel@rams-air.lan>
2023-08-23 23:01:59 -0700

    added back .value. otherwise it's just a input type obj

--- button_script.js
+++ button_script.js
@@ -3,6 +3,6 @@ const submitButton = document.getElementById("submitButton");
 const output = document.getElementById("output");
 
 submitButton.addEventListener("click", function() {
-    const enteredNumber = numberInput;
+    const enteredNumber = numberInput.value;
     output.textContent = "You entered: " + enteredNumber;
 });
