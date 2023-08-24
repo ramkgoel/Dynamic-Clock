67182c7e4f64d5f80fc53e08d7b58af26ae2d5c5
Ram Goel <ramgoel@rams-air.lan>
2023-08-23 23:01:27 -0700

    removed .value from numberInput

--- button_script.js
+++ button_script.js
@@ -3,6 +3,6 @@ const submitButton = document.getElementById("submitButton");
 const output = document.getElementById("output");
 
 submitButton.addEventListener("click", function() {
-    const enteredNumber = numberInput.value;
+    const enteredNumber = numberInput;
     output.textContent = "You entered: " + enteredNumber;
 });
